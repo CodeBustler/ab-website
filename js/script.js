9f9952a1c0fda5e3bd6e62581d5750eefe1fc71e
CodeBustler <codebustler360online@gmail.com>
2023-12-03 02:30:40 +0530

    FILES ADDED

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const copyrightYear = document.getElementById("copyrightYear");
+
+// Dynamic Year
+copyrightYear.innerHTML = new Date().getFullYear();
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }),
+);
